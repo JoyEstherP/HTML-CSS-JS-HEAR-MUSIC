05377b033be82fbe11a239138411ea699e590a2d
JoyEstherP <103353094+JoyEstherP@users.noreply.github.com>
2022-11-01 21:49:13 +0530

    MUSIC PLAYER

---
+++ music.js
@@ -0,0 +1,289 @@
+var songs=[
+    {
+        id: 1,
+        imageSrc:"G:/java script/website recreate/cini nsongs/images/vv.jpg",
+        audioSrc:"G:/java script/website recreate/cini nsongs/audios/New folder/02 - Anbil Avan.mp3",
+        title:"Anbil Avan",
+    },
+    {
+        id: 2,
+        imageSrc:"G:/java script/website recreate/cini nsongs/images/71UOz+oUpgS._RI_.jpg",
+        audioSrc:"G:/java script/website recreate/cini nsongs/audios/02- Munbe Vaa.mp3",
+        title:"Munbe Vaa",
+    },
+    {
+        id: 3,
+        imageSrc:"G:/java script/website recreate/cini nsongs/images/17o-kadhal.jpg",
+        audioSrc:"G:/java script/website recreate/cini nsongs/audios/Aye Sinamika.mp3",
+        title:"Aye Sinamika",
+    },
+    {
+        id: 4,
+        imageSrc:"G:/java script/website recreate/cini nsongs/images/1c78e1aaffd2b3021d919774a935da21.jpg",
+        audioSrc:"G:/java script/website recreate/cini nsongs/audios/01 Endrendrum Punnagai.mp3",
+        title:"Endrendrum Punnagai",
+    },
+    {
+        id: 5,
+        imageSrc:"G:/java script/website recreate/cini nsongs/images/bombay-1.jpg",
+        audioSrc:"G:/java script/website recreate/cini nsongs/audios/01 Antha Arabi.mp3",
+        title:"Antha Arabi",
+        
+    },
+    {
+        id: 6,
+        imageSrc:"G:/java script/website recreate/cini nsongs/images/d.jpg",
+        audioSrc:"G:/java script/website recreate/cini nsongs/audios/Kaattu Payale.mp3",
+        title:"Kaattu Payale",
+    },
+   
+    {
+        id: 7,
+        imageSrc:   "G:/java script/website recreate/cini nsongs/images/71UOz+oUpgS._RI_.jpg",
+        audioSrc:"G:/java script/website recreate/cini nsongs/audios/05- New York.mp3",
+        title:"New York",
+        
+    },
+    {
+        id: 8,
+        
+        imageSrc:"G:/java script/website recreate/cini nsongs/images/vv.jpg",
+        audioSrc:"G:/java script/website recreate/cini nsongs/audios/New folder/06 - Mannipaaya.mp3",
+        title:"Mannipaaya",
+    },
+    {
+        id: 9,
+        imageSrc:   "G:/java script/website recreate/cini nsongs/images/1c78e1aaffd2b3021d919774a935da21.jpg",
+        audioSrc:"G:/java script/website recreate/cini nsongs/audios/07 Mangalyam.mp3",
+        title:"Mangalyam",
+    },
+    {
+        id: 10,
+        imageSrc:   "G:/java script/website recreate/cini nsongs/images/17o-kadhal.jpg",
+        audioSrc:"G:/java script/website recreate/cini nsongs/audios/Mental Manadhil.mp3",
+        title:"Mental Manadhil",
+    },
+    {
+        id: 11,
+        imageSrc:   "G:/java script/website recreate/cini nsongs/images/ennai ko.jpg",
+        audioSrc:"G:/java script/website recreate/cini nsongs/audios/ennai kolathey.mp3",
+        title:"Ennai Kollathey",
+        
+    },
+    {
+        id: 12,
+        imageSrc:   "G:/java script/website recreate/cini nsongs/images/beast.jpg",
+        audioSrc:"G:/java script/website recreate/cini nsongs/audios/Jolly O Gymkhana Beast 128 Kbps.mp3",
+        title:"Jolly O ",
+    },
+    {
+        id: 13,
+        imageSrc:"G:/java script/website recreate/cini nsongs/images/ps.jpg",
+        audioSrc:"G:/java script/website recreate/cini nsongs/audios/ponni Nadhi.mp3",
+        title:"ponni Nadhi",
+        
+    },
+    {
+        id: 14,
+        imageSrc:   "G:/java script/website recreate/cini nsongs/images/vv.jpg",
+        audioSrc:"G:/java script/website recreate/cini nsongs/audios/New folder/03 - Vinnathaandi Varuvaayaa.mp3",
+        title:"Vinnathaandi Varuvaayaa",
+    },
+    {
+        id: 15,
+        imageSrc:   "G:/java script/website recreate/cini nsongs/images/bombay-1.jpg",
+        audioSrc:"G:/java script/website recreate/cini nsongs/audios/02 Poovukku Enna.mp3",
+        title:"Poovukku Enna",
+        
+    },
+    {
+        id: 16,
+        
+        imageSrc:   "G:/java script/website recreate/cini nsongs/images/1c78e1aaffd2b3021d919774a935da21.jpg",
+        audioSrc:"G:/java script/website recreate/cini nsongs/audios/02 Pachchai Nirame.mp3",
+        title:"Pachchai Nirame",
+        
+    },
+    {
+        id: 17,
+        imageSrc:   "G:/java script/website recreate/cini nsongs/images/71UOz+oUpgS._RI_.jpg",
+        audioSrc:"G:/java script/website recreate/cini nsongs/audios/07- Jill Endru Oru Kaadhal.mp3",
+        title:"Jill Endru Oru Kaadhal",
+        
+    },
+    {
+        id: 18,
+        imageSrc:   "G:/java script/website recreate/cini nsongs/images/d.jpg",
+        audioSrc:"G:/java script/website recreate/cini nsongs/audios/Aagasam.mp3",
+        title:"Aagasam",
+    },
+    {
+        id: 19,
+        imageSrc:   "G:/java script/website recreate/cini nsongs/images/ps.jpg",
+        audioSrc:"G:/java script/website recreate/cini nsongs/audios/Ratchasa Maamaney.mp3",
+        title:"Ratchasa Maamaney",
+    },
+    {
+        id: 20,
+        imageSrc:   "G:/java script/website recreate/cini nsongs/images/17o-kadhal.jpg",
+        audioSrc:"G:/java script/website recreate/cini nsongs/audios/Naane Varugiraen.mp3",
+        title:"Naane Varugiraen",
+    },
+    {
+        id: 21,
+        imageSrc:"G:/java script/website recreate/cini nsongs/images/vv.jpg",
+        audioSrc:"G:/java script/website recreate/cini nsongs/audios/New folder/04 - Hosanna.mp3",
+        title:"Hosanna",
+    },
+    {
+        id: 22,
+        imageSrc:"G:/java script/website recreate/cini nsongs/images/bombay-1.jpg",
+        audioSrc:"G:/java script/website recreate/cini nsongs/audios/03 Uyire Uyire.mp3",
+        title:"Uyire Uyire",
+    },
+    {
+        id: 23,
+        imageSrc:"G:/java script/website recreate/cini nsongs/images/17o-kadhal.jpg",
+        audioSrc:"G:/java script/website recreate/cini nsongs/audios/Parandhu Sella Vaa.mp3",
+        title:"Parandhu Sella Vaa",
+    },
+    {
+        id: 24,
+        imageSrc:"G:/java script/website recreate/cini nsongs/images/d.jpg",
+        audioSrc:"G:/java script/website recreate/cini nsongs/audios/Naalu Nimisham.mp3",
+        title:"Naalu Nimisham",
+    },
+    {
+        id: 25,
+        imageSrc:"G:/java script/website recreate/cini nsongs/images/1c78e1aaffd2b3021d919774a935da21.jpg",
+        audioSrc:"G:/java script/website recreate/cini nsongs/audios/03 Kadhal Sadugudu.mp3",
+        title:"Kadhal Sadugudu",
+    },
+    {
+        id: 26,
+        imageSrc:"G:/java script/website recreate/cini nsongs/images/17o-kadhal.jpg",
+        audioSrc:"G:/java script/website recreate/cini nsongs/audios/Malargal Kaettaen.mp3",
+        title:"Malargal Kaettaen",
+    },
+   
+    {
+        id: 27,
+        imageSrc:"G:/java script/website recreate/cini nsongs/images/ps.jpg",
+        audioSrc:"G:/java script/website recreate/cini nsongs/audios/Sol.mp3",
+        title:"Sol",
+    },
+    {
+        id: 28,
+        imageSrc:"G:/java script/website recreate/cini nsongs/images/d.jpg",
+        audioSrc:"G:/java script/website recreate/cini nsongs/audios/Kayilae Aagasam.mp3",
+        title:"Kayilae Aagasam",
+    },
+    {
+        id: 29,
+        imageSrc:   "G:/java script/website recreate/cini nsongs/images/ps.jpg",
+        audioSrc:"G:/java script/website recreate/cini nsongs/audios/Chola Chola.mp3",
+        title:"Chola Chola",
+    },
+    {
+        id: 30,
+        imageSrc:   "G:/java script/website recreate/cini nsongs/images/bombay-1.jpg",
+        audioSrc:"G:/java script/website recreate/cini nsongs/audios/04 Kuchi Kuchi.mp3",
+        title:"Kuchi Kuchi",
+    },
+    {
+        id: 31,
+        imageSrc:   "G:/java script/website recreate/cini nsongs/images/1c78e1aaffd2b3021d919774a935da21.jpg",
+        audioSrc:"G:/java script/website recreate/cini nsongs/audios/05 Alaipayuthey.mp3",
+        title:"Alaipayuthey",
+    },
+    {
+        id: 32,
+        imageSrc:   "G:/java script/website recreate/cini nsongs/images/17o-kadhal.jpg",
+        audioSrc:"G:/java script/website recreate/cini nsongs/audios/Theera Ulaa.mp3",
+        title:"Theera Ulaa",
+    },
+    {
+        id: 33,
+        imageSrc:   "G:/java script/website recreate/cini nsongs/images/vv.jpg",
+        audioSrc:"G:/java script/website recreate/cini nsongs/audios/New folder/05 - Kannukkul Kannai.mp3",
+        title:"Kannukkul Kannai",
+    },
+    {
+        id: 34,
+        imageSrc:   "G:/java script/website recreate/cini nsongs/images/beast.jpg",
+        audioSrc:"G:/java script/website recreate/cini nsongs/audios/Arabic Kuthu .mp3",
+        title:"Arabic Kuthu",
+    },
+    {
+        id: 35,
+        imageSrc:   "G:/java script/website recreate/cini nsongs/images/d.jpg",
+        audioSrc:"G:/java script/website recreate/cini nsongs/audios/Veyyon Silli.mp3",
+        title:"Veyyon Silli",
+    },
+    {
+        id: 36,
+        imageSrc:   "G:/java script/website recreate/cini nsongs/images/bombay-1.jpg",
+        audioSrc:"G:/java script/website recreate/cini nsongs/audios/05 Kannalanae.mp3",
+        title:"Kannalanae",
+    },
+    {
+        id: 37,
+        imageSrc:   "G:/java script/website recreate/cini nsongs/images/1c78e1aaffd2b3021d919774a935da21.jpg",
+        audioSrc:"G:/java script/website recreate/cini nsongs/audios/04 Evano Oruvan.mp3",
+        title:"Evano Oruvan",
+    },
+    {
+        id: 38,
+        imageSrc:   "G:/java script/website recreate/cini nsongs/images/17o-kadhal.jpg",
+        audioSrc:"G:/java script/website recreate/cini nsongs/audios/Kaara Aattakkaara.mp3",
+        title:"Kaara Aattakkaara",
+    },
+    {
+        id: 39,
+        imageSrc:   "G:/java script/website recreate/cini nsongs/images/d.jpg",
+        audioSrc:"G:/java script/website recreate/cini nsongs/audios/Mannurunda.mp3",
+        title:"Mannurunda",
+    },
+    {
+        id: 40,
+        imageSrc:   "G:/java script/website recreate/cini nsongs/images/vv.jpg",
+        audioSrc:"G:/java script/website recreate/cini nsongs/audios/New folder/01 - Omana Penne.mp3",
+        title:"Omana Penne",
+    },
+   
+
+    
+]
+function generateAudioCard() {
+    let htmlStrings="";
+    songs.forEach(function(song,index){
+        let songHTML=`
+            <div class="audio">
+                <div class="audi_img">
+                    <img src="${song.imageSrc}">
+                    
+                </div>
+                <div class="audio_detail">
+                    <h1 class="audio_title">${song.title} 
+                    </h1>
+                    <button class="btn"onclick="playAudio(${song.id})">play</button>
+                </div>
+            </div>`;
+
+        htmlStrings+=songHTML;
+    })
+    document.getElementById("audios").innerHTML=htmlStrings;
+}
+generateAudioCard();
+
+function playAudio(songid){
+    var song=songs.find(function(song,index){
+        return song.id===songid
+    });
+    
+    
+    document.getElementById("thumbnail").src=song.imageSrc;
+    document.getElementById("audio_player").src=song.audioSrc;
+    document.getElementById("audio_player_title").innerText=song.title;
+    document.getElementById("audio_player").play();
+}
